fd8bb004a6cdd4dcd843b2537453698c0554dfa1
Bill <billchen314@gmail.com>
2019-07-23 16:11:04 -0500

    Start change for data saving

---
+++ app/src/main/java/com/example/bchen/billsapp/Word.java
@@ -0,0 +1,22 @@
+package com.example.bchen.billsapp;
+
+import androidx.room.ColumnInfo;
+
+import androidx.room.Entity;
+
+import androidx.room.PrimaryKey;
+
+import androidx.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
