45b1d64248d71526749dfdc62b6ea9be23860736
Bill <billchen314@gmail.com>
2019-07-27 18:23:04 -0500

    change finished data thingy 2.0

--- app/src/main/java/com/example/bchen/billsapp/WordViewHolder.java
+++ app/src/main/java/com/example/bchen/billsapp/WordViewHolder.java
@@ -1,15 +1,16 @@
 package com.example.bchen.billsapp;
 
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 public class WordViewHolder extends RecyclerView.ViewHolder {
     private TextView simpleTextView;
 
     public WordViewHolder(final View itemView, final ItemClickListener listener) {
         super(itemView);
-        simpleTextView = (TextView) itemView.findViewById(R.id.tvAnimalName);
+        simpleTextView = (TextView) itemView.findViewById(R.id.my_list_view);
         simpleTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
