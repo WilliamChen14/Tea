f5e57176576c65be135d463f6e68e8fab82b3ad5
Bill <billchen314@gmail.com>
2019-07-31 16:31:07 -0500

    change add camera function so that you can take pictures for profile pic

--- app/src/main/java/com/example/bchen/billsapp/NewWordActivity.java
+++ app/src/main/java/com/example/bchen/billsapp/NewWordActivity.java
@@ -1,19 +1,26 @@
 package com.example.bchen.billsapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewWordActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
+    private static final int CAMERA_PIC_REQUEST = 1337;
+
 
     private EditText mEditWordView;
+
+    private Bitmap imageData;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,5 +42,23 @@ public class NewWordActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        final ImageView profilePic =  findViewById(R.id.profilePicture);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                imageData = (Bitmap) data.getExtras().get("data");
+                ImageView image = (ImageView) findViewById(R.id.profilePicture);
+                image.setImageBitmap(imageData);
+            }
+        }
     }
 }
